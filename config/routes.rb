ed931ee15088529d37d91aa0213be35b243b9756
leeweiminsg <40821628+leeweiminsg@users.noreply.github.com>
2019-12-22 11:14:47 +0800

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  namespace :api do
+    jsonapi_resources :tasks
+    jsonapi_resources :tags
+  end
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+tasks = Task.create([{ title: 'Title 1', description: 'Description 1', is_deleted: false }, { title: 'Title 2', description: 'Description 2', is_deleted: false }])
+
+tags = Tag.create([{title: 'None', is_deleted: false}, {title: 'Home', is_deleted: false}, {title: 'School', is_deleted: false}, {title: 'Work', is_deleted: false}])
